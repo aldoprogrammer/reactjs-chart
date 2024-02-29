08665c37df1331095b9d31b0e3c7f7027ef0ba76
aldoprogrammer <aldobesma@gmail.com>
2024-02-29 21:50:12 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,85 @@
 import { useState } from 'react'
-import { Card, DonutChart, Metric, Text } from '@tremor/react';
+import { Card, DonutChart, LineChart , Metric, Text } from '@tremor/react';
 import barbieData from './movie-barbie.json'
 import aldoData from './movie-aldo.json'
 import './App.css'
+const chartData = barbieData.domestic_daily.map(({revenue, date}) => {
+  const aldoLSData = aldoData.domestic_daily.find(opp => opp.date === date)
+  return {
+    date,
+    Barbie: revenue,
+    Aldo: aldoLSData ? aldoLSData.revenue : 0
+  }
+})
+
+
+
+// const chartdata = [
+//   {
+//     date: 'Jan 22',
+//     SemiAnalysis: 2890,
+//     'The Pragmatic Engineer': 2338,
+//   },
+//   {
+//     date: 'Feb 22',
+//     SemiAnalysis: 2756,
+//     'The Pragmatic Engineer': 2103,
+//   },
+//   {
+//     date: 'Mar 22',
+//     SemiAnalysis: 3322,
+//     'The Pragmatic Engineer': 2194,
+//   },
+//   {
+//     date: 'Apr 22',
+//     SemiAnalysis: 3470,
+//     'The Pragmatic Engineer': 2108,
+//   },
+//   {
+//     date: 'May 22',
+//     SemiAnalysis: 3475,
+//     'The Pragmatic Engineer': 1812,
+//   },
+//   {
+//     date: 'Jun 22',
+//     SemiAnalysis: 3129,
+//     'The Pragmatic Engineer': 1726,
+//   },
+//   {
+//     date: 'Jul 22',
+//     SemiAnalysis: 3490,
+//     'The Pragmatic Engineer': 1982,
+//   },
+//   {
+//     date: 'Aug 22',
+//     SemiAnalysis: 2903,
+//     'The Pragmatic Engineer': 2012,
+//   },
+//   {
+//     date: 'Sep 22',
+//     SemiAnalysis: 2643,
+//     'The Pragmatic Engineer': 2342,
+//   },
+//   {
+//     date: 'Oct 22',
+//     SemiAnalysis: 2837,
+//     'The Pragmatic Engineer': 2473,
+//   },
+//   {
+//     date: 'Nov 22',
+//     SemiAnalysis: 2954,
+//     'The Pragmatic Engineer': 3848,
+//   },
+//   {
+//     date: 'Dec 22',
+//     SemiAnalysis: 3239,
+//     'The Pragmatic Engineer': 3736,
+//   },
+// ];
+
+const valueFormatter = function (number) {
+  return '$ ' + new Intl.NumberFormat('us').format(number).toString();
+};
 
 
 
@@ -21,6 +98,7 @@ function App() {
   }
   
   return (
+    <div>
   <div className='grid lg:grid-cols-2 gap-12
   grid-cols-1 max-w-screen-xl'>
     
@@ -166,6 +244,19 @@ function App() {
       
     </div>
   </div>
+  <Card className='mt-5'>
+     <h3 className="text-lg font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">Newsletter revenue over time (USD)</h3>
+      <LineChart
+        className="mt-4 h-72"
+        data={chartData}
+        index="date"
+        yAxisWidth={120}
+        categories={['Barbie', 'Aldo']}
+        colors={['indigo', 'cyan']}
+        valueFormatter={addCommasToNumber}
+      />
+    </Card>
+    </div>
   )
 }
 
