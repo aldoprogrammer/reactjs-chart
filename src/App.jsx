bc2ef76a39a18f141446e2ee0de9d6177332b4bc
aldoprogrammer <aldobesma@gmail.com>
2024-02-29 20:33:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+    <h2 className='text-lg text-blue-500'>Hello</h2>
+  </>
+  )
+}
+
+export default App
